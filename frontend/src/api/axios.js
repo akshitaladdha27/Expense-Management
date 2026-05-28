cb2d16f1655c0e2d0b41d908e86de6caaa09d978
akshitaladdha27 <akshitaladdha27@gmail.com>
2026-05-28 15:17:21 +0530

    adding changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://expense-management-1-e8gl.onrender.com/',
   headers: {
     "Content-Type": "application/json",
   },
